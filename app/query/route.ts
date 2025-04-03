cd3a3039bcc764afbd728319a11573ca5405d173
niral2611 <niral.ajmera@piersight.space>
2025-04-03 14:50:40 +0530

    module11

--- app/query/route.ts
+++ app/query/route.ts
@@ -2,7 +2,7 @@ import postgres from 'postgres';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
-export async function listInvoices() {
+async function listInvoices() {
 	const data = await sql`
     SELECT invoices.amount, customers.name
     FROM invoices
