4329fa4c8a4c0e21b7f091e704c0ff5c4109d597
niral2611 <niral.ajmera@piersight.space>
2025-04-03 18:13:40 +0530

    module16

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,24 @@
-export default function Page() {
-    return <p>Customers Page</p>;
-  }
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: { query?: string; page?: string };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const customers = await fetchFilteredCustomers(query, currentPage);
+
+  return (
+    <div>
+      <h1>Customers</h1>
+      <CustomersTable customers={customers} />
+    </div>
+  );
+}
